58fe60a735f910fb7fb07fce0fc888eadb6e6ceb
Pritisha Jain <pritisha.jain@intimetec.com>
2023-02-13 15:23:24 +0530

    week1_assignment: created a todo list

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Display from './components/to-do-display';
+
+function App() {
+  return (
+    <Display/>
+       );
+}
+
+export default App;
---
+++ to-do-list/src/components/to-do-display.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Display() {
+    return (
+        <>
+            <div className="main-div">
+                <div className='head'>
+                    <h1>ToDo List</h1>
+                    <i className="fa fa-trash" aria-hidden="true"></i>
+                </div>
+
+                <div className='container'>
+                    <div className="add-list">
+
+                        <input type="text" placeholder="Add a Item" id="input-text" />
+                        <button id="Add"> + </button>
+
+                    </div>
+                    <div className='show-list'>
+                        <h1>Your List is:</h1>
+                        <ol>
+
+                        </ol>
+
+                    </div>
+                    <div className='remove-list'>
+                        <button id="remove">Remove All</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+export default Display;

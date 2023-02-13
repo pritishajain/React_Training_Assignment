d3230169c90098f963e276fcc8c80d0eec26d422
Pritisha Jain <pritisha.jain@intimetec.com>
2023-02-13 19:33:44 +0530

    week1_assignment: removed the unwanted favicons and edited the folder structure

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Display from './components/to-do-display';
 
 function App() {
--- to-do-list/src/components/to-do-display.tsx
+++ to-do-list/src/components/to-do-display.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../assets/css/to-do-display.css';
 
 
 function Display() {

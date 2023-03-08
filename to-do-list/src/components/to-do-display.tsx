73ddeee8fe0743a7d982d9cffa34dfd7d6d0a10e
Pritisha Jain <pritisha.jain@intimetec.com>
2023-03-08 14:08:39 +0530

    resolved conflicts

--- to-do-list/src/components/to-do-display.tsx
+++ to-do-list/src/components/to-do-display.tsx
@@ -5,7 +5,6 @@ import NavBar from "./nav-bar";
 import { Heading, Placeholder, Active, Completed, RemoveAll, Add, Colon } from "../assets/constants/constant";
 import { Link } from "react-router-dom";
 
-<<<<<<< Updated upstream
 type myState = {
   data: string;
   active: { id: string; title: string }[];
@@ -14,34 +13,14 @@ type myState = {
 };
 
 class TodoDisplay extends React.Component<{}, myState> {
-=======
-class TodoDisplay extends React.Component<
-  {},
-  {
-    data: string;
-    items: { id: string; title: string }[];
-    completed: { id: string; title: string }[];
-    recycleBin: { id: string; title: string }[];
-    
-  }
-> {
->>>>>>> Stashed changes
   constructor(props: string) {
     super(props);
 
     this.state = {
-<<<<<<< Updated upstream
       active: [],
       data: "",
       complete: [],
       recycleBin: [],
-=======
-      items: [],
-      data: "",
-      completed: [],
-      recycleBin: [],
-      
->>>>>>> Stashed changes
     };
   }
 
@@ -98,63 +77,6 @@ class TodoDisplay extends React.Component<
   };
 
   render() {
-<<<<<<< Updated upstream
-=======
-    const itemEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
-      this.setState({
-        data: event.target.value,
-      });
-    };
-
-    const addItem = () => {
-      if (!this.state.data.length || this.state.data.trim().length == 0) {
-        return;
-      }
-      let newTask: { id: string; title: string } = {
-        title: this.state.data,
-        id: new Date().getTime().toString(),
-      };
-
-      this.setState({
-        items: [...this.state.items, newTask],
-       
-        data: "",
-      });
-    };
-
-    const removeItem = (itemid: string, text: string, e: any) => {
-      let newTask: { id: string; title: string } = {
-        title: text,
-        id: itemid,
-      };
-
-      this.setState({
-        items: [...this.state.items].filter((element) => itemid !== element.id),
-        recycleBin: [...this.state.recycleBin, newTask],
-      });
-    };
-
-    const removeAllItems = () => {
-      this.setState({
-        recycleBin:[...this.state.recycleBin,...this.state.items],
-        items: [],
-
-      });
-    };
-
-    const completedItem = (itemid: string, text: string, e: any) => {
-      let newTask: { id: string; title: string } = {
-        title: text,
-        id: itemid,
-      };
-
-      this.setState({
-        items: [...this.state.items].filter((element) => itemid !== element.id),
-        completed: [...this.state.completed, newTask],
-       
-      });
-    };
->>>>>>> Stashed changes
     return (
       <React.Fragment>
         <div className="main-div">
@@ -172,7 +94,6 @@ class TodoDisplay extends React.Component<
               />
             </div>
 
-<<<<<<< Updated upstream
             <div className="upper-container">
               <div className="add-list">
                 <input
@@ -255,30 +176,6 @@ class TodoDisplay extends React.Component<
                   </Link>
                 </span>
               </div>
-=======
-            <div className="show-list">
-              <ul>
-                {this.state.items.map((value) => {
-                  return (
-                    <ToDoList
-                      text={value.title}
-                      itemid={value.id}
-                      onSelect={removeItem}
-                      onChange={completedItem}
-                    />
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-          <div className="lower-container">
-            <div className="active">Active :{this.state.items.length}</div>
-            <div className="active">
-              Completed : {this.state.completed.length}
-            </div>
-            <div className="active" onClick={removeAllItems}>
-              Remove All
->>>>>>> Stashed changes
             </div>
             <div className="active">
               <i className="fa fa-trash"></i>
--- to-do-list/src/components/to-do-list.tsx
+++ to-do-list/src/components/to-do-list.tsx
@@ -6,13 +6,8 @@ const ToDoList = (props: {
   key: string;
   text: string;
   itemid: string;
-<<<<<<< Updated upstream
   onSelect: (itemid: string, text: string) => void;
   onChange: (itemid: string, text: string) => void;
-=======
-  onSelect: (itemid: string, text: string, e: any) => void;
-  onChange: (itemid: string, text: string, e: any) => void;
->>>>>>> Stashed changes
 }) => {
   
   return (
@@ -22,7 +17,6 @@ const ToDoList = (props: {
         <i
           className="fa fa-check"
           aria-hidden="true"
-<<<<<<< Updated upstream
           onClick={() => {props.onChange(props.itemid, props.text);}} 
         >
           <span className="tooltip">{Complete}</span>
@@ -34,22 +28,6 @@ const ToDoList = (props: {
           onClick={() => {props.onSelect(props.itemid, props.text);}} 
         >
           <span className="tooltip">{Delete}</span>
-=======
-          onClick={(e: any) => {
-            props.onChange(props.itemid, props.text, e);
-          }}
-        >
-          <span className="tooltip">Completed</span>
-        </i>
-        <i
-          className="fa fa-times"
-          aria-hidden="true"
-          onClick={(e: any) => {
-            props.onSelect(props.itemid, props.text, e);
-          }}
-        >
-          <span className="tooltip">Deleted</span>
->>>>>>> Stashed changes
         </i>
       </div>
     </React.Fragment>
